26ce64a9f2aaddddbed25835cb32239821bc29fe
Kakon Debnath <kakondebnath96@gmail.com>
2023-06-09 18:47:36 +0600

    update instructor classes table add some color

--- src/pages/Dashboard/Instracor/MyClassesCard.jsx
+++ src/pages/Dashboard/Instracor/MyClassesCard.jsx
@@ -6,56 +6,58 @@ const MyClassesCard = ({ classes }) => {
     console.log(classes);
     return (
         <div className="overflow-x-auto">
-
             {
                 classes && Array.isArray(classes) && classes.length > 0 ?
-                    <>
-                        <table className="table">
-                            {/* head */}
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Image</th>
-                                    <th>Class Name</th>
-                                    <th>Available Seats</th>
-                                    <th>Price</th>
-                                    <th>Status</th>
-                                    <th>Enrolled Student</th>
-                                    <th>Feedback</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                                {
-                                    classes.map((singleClass, i) =>
-                                        <tr key={i}>
-                                            <td>{i + 1}</td>
-                                            <td>
-                                                <div className="avatar">
-                                                    <div className="mask mask-squircle w-12 h-12">
-                                                        <img src={singleClass?.image} alt="Avatar Tailwind CSS Component" />
-                                                    </div>
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr className='bg-base-300'>
+                                <th>#</th>
+                                <th>Image</th>
+                                <th>Class Name</th>
+                                <th>Available Seats</th>
+                                <th>Price</th>
+                                <th>Status</th>
+                                <th>Enrolled Student</th>
+                                <th>Feedback</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                classes.map((singleClass, i) =>
+                                    <tr key={i} className='transition-all duration-500 hover:bg-base-200'>
+                                        <td>{i + 1}</td>
+                                        <td>
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={singleClass?.image} alt="Avatar Tailwind CSS Component" />
                                                 </div>
-                                            </td>
-                                            <td>{singleClass?.className}</td>
-                                            <td>{singleClass?.availableSeats}</td>
-                                            <td>$ {singleClass?.price}</td>
-                                            <td>{singleClass?.status}</td>
-                                            <td>{singleClass?.enrolledStudent || 0}</td>
-                                            <td>{singleClass?.feedback || "Not Available"}</td>
-                                            <td>
-                                                <div className="tooltip hover:tooltip-open tooltip-top mx-2" data-tip="Edit Class">
-                                                    <button className="text-blue-500 p-2 "><FaPencilAlt size={18}></FaPencilAlt>
-                                                    </button>
-                                                </div>
-                                            </td>
-                                        </tr>)
-                                }
-                            </tbody>
-                        </table>
-
-                    </> :
+                                            </div>
+                                        </td>
+                                        <td className='capitalize'>{singleClass?.className}</td>
+                                        <td className='text-center'>{singleClass?.availableSeats}</td>
+                                        <td>$ {singleClass?.price}</td>
+                                        <td className={`capitalize`}>
+                                            <p className={`text-white text-center rounded-lg ${singleClass?.status === "pending" ? "bg-yellow-500" : singleClass?.status === "approved" ? "bg-green-500" : "bg-red-500 "}`}>{singleClass?.status}</p>
+                                        </td>
+                                        <td className='text-center'>{singleClass?.enrolledStudent || 0}</td>
+                                        <td>
+                                            <p className={`${singleClass?.adminFeedback && "bg-red-500 text-white text-center rounded-md"}`}>
+                                                {singleClass?.adminFeedback || "Not Available"}
+                                            </p>
+                                        </td>
+                                        <td>
+                                            <div className="tooltip hover:tooltip-open tooltip-top mx-2" data-tip="Edit Class">
+                                                <button className="text-blue-500 p-2 "><FaPencilAlt size={18}></FaPencilAlt>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>)
+                            }
+                        </tbody>
+                    </table>
+                    :
                     <>
                         <EmptyState message={"No Data Available"}></EmptyState>
                     </>
